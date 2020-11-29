4d9a0ce971449d37738a28d739a7b0def022a195
hodorwho (LISE) <43373910+hodorwho@users.noreply.github.com>
2020-11-29 23:07:33 +0100

    sd

--- littjs.js
+++ littjs.js
@@ -1,30 +1,14 @@
 
 var tall = 1;
 
-function overflowx(){
+function overflowx(x){
     tall+=1;
     if (tall%2 == 0){
         document.body.style.overflowY = "hidden";
+        tabindex = 0;
     }
     else{
         document.body.style.overflowY = "visible";
     }
 
 }
-
-function handleBtnKeyDown() {
-    if (event.key === "" || event.key === "Spacebar" ||event.key === "Enter") {
-       // prevent default action to stop scrolling when space is pressed
-      event.preventDefault();
-      overflowx();
-      toggleButton(event.target);
-    }
-  }
-
-function toggleButton(button) {
-    var pressed = (button.getAttribute("aria-pressed") === "true");
-    button.setAttribute("aria-pressed", !pressed);
-    //button.setAttribute('aria-pressed', isAriaPressed ? 'false' : 'true');
-  }
-
-  //document.body.style.overflowY = aria-hidden === "true";
