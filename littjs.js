f5b52a9c4ff03c45f8179d05d008ae34a13741a3
hodorwho (LISE) <43373910+hodorwho@users.noreply.github.com>
2020-11-29 17:27:24 +0100

    Update littjs.js

--- littjs.js
+++ littjs.js
@@ -12,3 +12,9 @@ function overflowx(){
     }
 
 }
+
+function toggleButton(button) {
+    var pressed = (button.getAttribute("aria-pressed") === "true");
+    button.setAttribute("aria-pressed", !pressed);
+    //button.setAttribute('aria-pressed', isAriaPressed ? 'false' : 'true');
+  }
